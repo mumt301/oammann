4ca06805a9f0043fa168eb6a6b5f01932fc32717
owenammann <owammann@gmail.com>
2020-12-12 22:36:26 -0500

    added musical notes

---
+++ Final Project/FinalProjectScript.js
@@ -0,0 +1,32 @@
+//Following this tutorial for the virtual piano
+//https://www.freecodecamp.org/news/javascript-piano-keyboard/
+//https://www.youtube.com/watch?v=vjco5yKZpU8
+
+playKeyboard();
+
+var keyboard = {
+    //Q
+    81: 'C,0',
+    //2
+    50: 'C#,0',
+    //W
+    87: 'D,0',
+    //3
+    51: 'D#,0',
+    //E
+    69: 'E,0',
+    //F
+    82: 'F,0',
+    //5
+    53: 'F#,0',
+    //T
+    84: 'G,0',
+    //6
+    54: 'G#,0',
+    //Y
+    89: 'A,0',
+    //7
+    55: 'A#,0',
+    //Y
+    85: 'B,0',
+}
